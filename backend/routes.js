011c985576c7498a735b15524989d6105f6227da
IzykPringle <ijpringl@mtu.edu>
2023-04-03 13:21:37 -0600

    to level 5

--- backend/routes.js
+++ backend/routes.js
@@ -12,12 +12,43 @@ const db = knex(config);
 
 routePath.get('/', (req, res) => {
     db
-    .select('*')
-    .from('movies')
-    .then(movies => {
-        let titles = movies.map(movie => movie.title)
-        res.json(titles)
-    })
+        .select('*')
+        .from('movies')
+        .then(movies => {
+            res.json(movies)
+        })
 })
+
+routePath.post('/', async (req, res) => {
+    let movie = req.body;
+    await db
+        .insert(movie)
+        .into('movies')
+        .then(function (result) {
+            res.json({ success: true, message: 'movie added' });
+        })
+        .catch(err =>
+            res.json({
+                message:
+                    'The data you are looking for could not be found. Please try again.'
+            })
+        );
+})
+
+routePath.delete('/', (req, res) => {
+    db('movies')
+        .where('title', req.body.title)
+        .del()
+        .then(function (result) {
+            res.json({ success: true, message: 'movie deleted' });
+        })
+        .catch(err =>
+            res.json({
+                message:
+                    'Could not delete this item.'
+            })
+        );
+})
+
 
 export default routePath;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,88 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
 
-  const movies = [
-    { title: 'Mean Girls' },
-    { title: 'Hackers' },
-    { title: 'The Grey' },
-    { title: 'Sunshine' },
-    { title: 'Ex Machina' },
-  ];
+  const [movies, setMovies] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [addedMovie, setAddedMovie] = useState("");
+  const [filteredMovies, setFilteredMovies] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/')
+      .then(response => response.json())
+      .then(data => {
+        setMovies(data)
+      })
+  }, [])
+
+
+  const addMovie = () => {
+    fetch('http://localhost:8080', {
+      method: "POST",
+      headers: {
+        'Content-Type': "application/json",
+      },
+      body: JSON.stringify({ title: addedMovie })
+    })
+  }
+
+  const deleteMovie = (id) => {
+    fetch('http://localhost:8080', {
+      method: "DELETE",
+      headers: {
+        'Content-Type': "application/json",
+      },
+      body: JSON.stringify({ title: id })
+    })
+  }
+
+
+
   return (
     <>
       <div className="App">
+        
         Movies
-        {movies.map(movie => <ul> {movie.title}</ul>)}
+        {movies.map(movie => <ul key={movie.id}>
+          {movie.title}
+          <button onClick={() => { deleteMovie(movie.title) }}>Delete</button>
+          <label for='watch'>Watched:</label>
+          <input id='watch' type="checkbox" onClick={() => {  }}/>
+        </ul>)}
       </div>
+      <div>
+        <input
+          type="search"
+          placeholder="Search here"
+          id="baseSearch"
+          onChange={event => { setSearchInput(event.target.value) }}
+          value={searchInput}>
+        </input>
+        <button onClick={() => { setFilteredMovies() }}>Search</button>
+
+        {movies.filter((movie) => {
+          if (searchInput === "") {
+            return null;
+          } else if (movie.title.toLowerCase().includes(searchInput.toLowerCase())) {
+            //setFilteredMovies(movie.title)
+            return movie.title;
+          }
+        }).map((movie) => {
+          return (
+            <div key={movie.id}>
+              <button className="searchButton" onClick={() => {
+                setSearchInput("")
+                setFilteredMovies(movie.title)
+                // navigate(`${base.Name}/equipment`)
+              }}>{movie.title}</button>
+            </div>
+          )
+        })
+        }
+      </div>
+      <input placeholder="add movie title" onChange={e => setAddedMovie(e.target.value)} />
+      <button onClick={() => { addMovie() }}>Add Movie</button>
     </>
   );
 }
